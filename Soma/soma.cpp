89d68587af21692c64e4411c24eaa9967a7dba66
Rogério Teixeira <rogerio.teixeira@inspira.com.br>
2018-12-29 22:37:36 -0200

    Exercicio de soma adicionado com solução em c++

---
+++ Soma/soma.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int soma = 0;
+    for(int i=1;i<=n;i++){
+        int x;
+        cin >> x;
+
+        soma += x;
+    }
+    cout << soma << endl;
+}
